7924d33a86df26cc283ee3ff4e8502fd927850fd
lucaired <aired@posteo.hu>
2022-12-19 16:13:12 +0100

    Tree builder: remove children for nodes and refactor Decision Tree handler

--- src/components/DecisionTreeHandler.tsx
+++ src/components/DecisionTreeHandler.tsx
@@ -1,82 +1,13 @@
 import {useState} from 'react';
 import Tree from "react-d3-tree";
-
-type DecisionTree = { id: any; showNodeControl: any; children: any };
-
-const setNodeProperty = (tree: DecisionTree, id: number, func: (tree: DecisionTree) => void) => {
-    if (tree.id === id) {
-        func(tree)
-    } else {
-        if (tree.children) {
-            tree.children.map((child: any) => setNodeProperty(child, id, func))
-        }
-    }
-}
-
-const setParentNodeProperty = (tree: DecisionTree, id: number, func: (tree: DecisionTree) => void) => {
-    if (tree.children){
-        if (tree.children.some((child: DecisionTree) => child.id === id)) {
-            func(tree)
-        } else {
-            if (tree.children) {
-                tree.children.map((child: any) => setParentNodeProperty(child, id, func))
-            }
-        }
-    }
-}
-
-
-const setNodeControl = (tree: DecisionTree) => {
-    tree.showNodeControl = !tree.showNodeControl;
-}
-
-const addNodeChild = (tree: DecisionTree) => {
-    // TODO: make this collision-proof
-    const id = (Math.random() + 1).toString(36).substring(7);
-    const child = {
-        name: 'Wood frame 1',
-        attributes: {
-            level: 'Building Part Type',
-        },
-        id: id,
-        showNodeControl: false,
-    }
-    tree.children = tree.children ? [...tree.children, child] : [child]
-}
-
-// @ts-ignore
-function RenderNode({nodeDatum, toggleNode, foreignObjectProps, handleNodeControl, handleAddChild, handleAddSibling}) {
-    return (
-        <g>
-            <rect
-                width="20"
-                height="20"
-                x="-10"
-                onClick={() => {
-                    handleNodeControl(nodeDatum.id);
-                    toggleNode();
-                }}
-            />
-            <foreignObject {...foreignObjectProps}>
-                <p>{nodeDatum.name}</p>
-                {nodeDatum.showNodeControl &&
-                    <button onClick={() => {
-                        handleNodeControl(nodeDatum.id);
-                        handleAddChild(nodeDatum.id);
-                    }}>↓</button>}
-                {nodeDatum.showNodeControl &&
-                    <button onClick={() => {
-                        handleNodeControl(nodeDatum.id);
-                        handleAddSibling(nodeDatum.id);
-                    }}>↔</button>}
-                {nodeDatum.showNodeControl && !nodeDatum.children &&
-                    <button onClick={() => {
-                        handleNodeControl(nodeDatum.id);
-                    }}>X</button>}
-            </foreignObject>
-        </g>
-    )
-}
+import {
+    addNodeChild,
+    removeNodeChild,
+    RenderNode,
+    setNodeControl,
+    setNodeProperty,
+    setParentNodeProperty
+} from "./NodeHandler";
 
 function DecisionTreeHandler() {
     const [decisionTree, setDecisionTree] = useState({
@@ -114,18 +45,22 @@ function DecisionTreeHandler() {
         ],
     })
 
-    const handleNodeControl = (id: number) => {
+    const handleNodeControl = (nodeId: string) => {
         let tree = {...decisionTree};
-        setNodeProperty(tree, id, setNodeControl)
+        setNodeProperty(tree, nodeId, setNodeControl)
         setDecisionTree(tree)
     }
-    const handleAddChild = (nodeId: number) => {
+    const handleAddChild = (nodeId: string) => {
         let tree = {...decisionTree};
         setNodeProperty(tree, nodeId, addNodeChild)
         setDecisionTree(tree)
     }
-
-    const handleAddSibling = (nodeId: number) => {
+    const handleRemoveChild = (nodeId: string) => {
+        let tree = {...decisionTree};
+        setParentNodeProperty(tree, nodeId, removeNodeChild)
+        setDecisionTree(tree)
+    }
+    const handleAddSibling = (nodeId: string) => {
         let tree = {...decisionTree};
         setParentNodeProperty(tree, nodeId, addNodeChild)
         setDecisionTree(tree)
@@ -138,10 +73,10 @@ function DecisionTreeHandler() {
         <Tree
             data={decisionTree}
             orientation={"vertical"}
-            dimensions={{
-                "width": 1200,
-                "height": 800
-            }}
+            // dimensions={{
+            //    "width": 1200,
+            //    "height": 800
+            // }}
             renderCustomNodeElement={(rd3tProps) =>
                 RenderNode({
                     ...rd3tProps,
@@ -149,6 +84,7 @@ function DecisionTreeHandler() {
                     handleNodeControl: handleNodeControl,
                     handleAddChild: handleAddChild,
                     handleAddSibling: handleAddSibling,
+                    handleRemoveChild: handleRemoveChild,
                 })
             }
         />
---
+++ src/components/NodeHandler.tsx
@@ -0,0 +1,96 @@
+export type DecisionTree = { id: any; showNodeControl: any; children: any };
+
+export const setNodeProperty = (tree: DecisionTree, id: string, func: (tree: DecisionTree) => void) => {
+    if (tree.id === id) {
+        func(tree)
+    } else {
+        if (tree.children) {
+            tree.children.map((child: any) => setNodeProperty(child, id, func))
+        }
+    }
+}
+export const setParentNodeProperty = (tree: DecisionTree, id: string, func: (tree: DecisionTree, id?: string) => void) => {
+    if (tree.children) {
+        if (tree.children.some((child: DecisionTree) => child.id === id)) {
+            func(tree)
+        } else {
+            if (tree.children) {
+                tree.children.map((child: any) => setParentNodeProperty(child, id, func))
+            }
+        }
+    }
+}
+export const setNodeControl = (tree: DecisionTree) => {
+    tree.showNodeControl = !tree.showNodeControl;
+}
+export const addNodeChild = (tree: DecisionTree) => {
+    // TODO: make this collision-proof
+    const id = (Math.random() + 1).toString(36).substring(7);
+    const child = {
+        name: `Wood frame ${id}`,
+        attributes: {
+            level: 'Building Part Type',
+        },
+        id: id,
+        showNodeControl: false,
+    }
+    tree.children = tree.children ? [...tree.children, child] : [child]
+}
+export const removeNodeChild = (tree: DecisionTree, nodeId?: string) => {
+    if (tree.children) {
+        const childIndex = tree.children.indexOf((child: DecisionTree) => child.id == nodeId, 0)
+        tree.children.splice(childIndex, 1)
+        if (tree.children.lenght == 0) {
+            tree.children = undefined
+        }
+    }
+}
+
+export function RenderNode({
+   // @ts-ignore
+   nodeDatum,
+   // @ts-ignore
+   toggleNode,
+   // @ts-ignore
+   foreignObjectProps,
+                               // @ts-ignore
+   handleNodeControl,
+                               // @ts-ignore
+   handleAddChild,
+                               // @ts-ignore
+   handleAddSibling,
+                               // @ts-ignore
+   handleRemoveChild
+}) {
+    return (
+        <g>
+            <rect
+                width="20"
+                height="20"
+                x="-10"
+                onClick={() => {
+                    handleNodeControl(nodeDatum.id);
+                    toggleNode();
+                }}
+            />
+            <foreignObject {...foreignObjectProps}>
+                <p>{nodeDatum.name}</p>
+                {nodeDatum.showNodeControl &&
+                    <button onClick={() => {
+                        handleNodeControl(nodeDatum.id);
+                        handleAddChild(nodeDatum.id);
+                    }}>↓</button>}
+                {nodeDatum.showNodeControl &&
+                    <button onClick={() => {
+                        handleNodeControl(nodeDatum.id);
+                        handleAddSibling(nodeDatum.id);
+                    }}>↔</button>}
+                {nodeDatum.showNodeControl && !nodeDatum.children &&
+                    <button onClick={() => {
+                        handleNodeControl(nodeDatum.id);
+                        handleRemoveChild(nodeDatum.id);
+                    }}>X</button>}
+            </foreignObject>
+        </g>
+    )
+}

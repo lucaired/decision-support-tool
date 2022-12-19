95eda98d44348310f49136318dc49fe677108819
lucaired <aired@posteo.hu>
2022-12-19 16:13:15 +0100

    Adapt neo4 docker-compose entry

--- backend/app/routers/ifc.py
+++ backend/app/routers/ifc.py
@@ -1,20 +1,18 @@
 from fastapi import APIRouter,  BackgroundTasks, UploadFile, status, File
 from tempfile import SpooledTemporaryFile, NamedTemporaryFile
 
-#from app.transform_ifc import run
-
 
 router = APIRouter(
     prefix="/ifc",
 )
 
 def transform_ifc_to_neo4j(file: SpooledTemporaryFile, file_name: str):
+    from app.transform_ifc import run
     named_temp_file = NamedTemporaryFile()
     content = file.read()
     named_temp_file.write(content)
     try:
-        pass
-        #run(named_temp_file.name, file_name)
+        run(named_temp_file.name, file_name)
         # TODO: send notification
     except Exception as e:
         # TODO: send notification
